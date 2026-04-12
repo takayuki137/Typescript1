a4529527c729474e59c37114b9b04984ae6944ef
Takayuki Hashimoto <taka.pioneer@gmail.com>
2026-04-13 00:31:53 +0900

    更新機能の修正

--- app/api/posts/[postId]/route.ts
+++ app/api/posts/[postId]/route.ts
@@ -33,7 +33,7 @@ export async function DELETE(
 
 export async function PATCH(
   req: Request,
-  { params }: { params: { postId: string } }
+  { params }: { params:Promise< { postId: string }> }
 ) {
   const { postId } = await params;
   const id = Number(postId);
